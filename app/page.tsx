9b1a0f32cb0d693e9359c51e1fa15efc59f88c6c
HenryWHU <160107279+HenryMoreau@users.noreply.github.com>
2024-07-10 13:47:44 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -33,26 +33,26 @@ export default function Home() {
               className="font-semibold relative z-10 after:absolute after:top-[50%] after:bg-mark
                         after:w-[100%] after:h-[50%] after:left-0 after:z-[-1]"
             >
-              Redish101
+              Henry Moreau
             </span>
             .
           </div>
         </div>
         <div className="text-2xl mt-3 text-light font-light">
-          Student / Developer
+          Student / Designer / Dreamchaser
         </div>
         <div className="flex text-2x mt-3 text-lightest font-light">
           <FontAwesomeIcon icon={faEnvelope} width={"16px"} />
-          <span className="ml-2">i@redish101.top</span>
-          <FontAwesomeIcon icon={faKey} width={"16px"} className="ml-3" />
-          <span className="ml-2">8678-69A4</span>
+          <span className="ml-2">me@henrywhu.cn</span>
+          <FontAwesomeIcon icon={faTwitter} width={"16px"} className="ml-3" />
+          <span className="ml-2">@HenryWHU_</span>
         </div>
         <div className="mt-4">
           <span>
-          <ButtonPrimary text="Blog" link="https://blog.redish101.top" />
+          <ButtonPrimary text="Blog" link="https://blog.henrywhu.cn" />
           </span>
           <span className="ml-2">
-            <ButtonLight text="GitHub" link="https://github.com/Redish101" />
+            <ButtonLight text="GitHub" link="https://github.com/HenryMoreau" />
           </span>
         </div>
       </div>
